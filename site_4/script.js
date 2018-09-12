a1334dfbd55b38a254d166796fbec8b2bd7ad212
github_samardoha <samardoha@domain.com>
2018-09-12 22:36:01 +0200

    Update6

--- site_4/script.js
+++ site_4/script.js
@@ -4,8 +4,9 @@
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i<names.length; i++)
     {
-    var res = names[i].charAt(0);
-    if (res=='J')
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    var res = names[i].charAt(0).toLowerCase();;
+    if (res==='j')
     {
       byeSpeaker.speak(names[i]);
     }
